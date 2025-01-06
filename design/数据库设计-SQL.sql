0728abc695162723aa3f95787f2d7b9fdd16ecb7
zhihang <akynazh@gmail.com>
2025-01-06 18:44:07 +0800

    feat: db design: sql v1

--- design/数据库设计-SQL.sql
+++ design/数据库设计-SQL.sql
@@ -1,14 +1,42 @@
 CREATE DATABASE IF NOT EXISTS aws;
 
-CREATE t_farm (
-    id INT PRIMARY KEY,
-    name VARCHAR(255),
-    address VARCHAR(255)
-)
-
-CREATE t_user (
+CREATE t_employee (
     id INT PRIMARY KEY,
     name VARCHAR(255),
     password VARCHAR(255),
-    farm_id INT
+);
+CREATE t_admin (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    password VARCHAR(255)
+);
+CREATE t_scale (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    key VARCHAR(255)
+);
+CREATE t_produce (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+CREATE t_work (
+    id INT PRIMARY KEY,
+    produce_id INT,
+    start_time INT,
+    end_time INT,
+    result INT
+);
+CREATE t_record (
+    id INT PRIMARY KEY,
+    work_id INT,
+    employee_id INT,
+    scale_id INT,
+    record_time INT,
+    record_data DOUBLE
+);
+
+CREATE t_work_dis (
+    id INT PRIMARY KEY,
+    work_id INT,
+    employee_id INT
 );
