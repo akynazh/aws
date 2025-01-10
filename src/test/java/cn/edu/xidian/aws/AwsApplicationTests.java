19c636227fd77c52b8092e32c397218df8420e54
zhihang <akynazh@gmail.com>
2025-01-10 14:35:58 +0800

    feat: init proj

---
+++ src/test/java/cn/edu/xidian/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.xidian.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
